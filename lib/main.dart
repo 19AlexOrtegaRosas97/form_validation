bfb909dcb00561b3cf6f452f0fdb5acd85f7ead3
Alejandro Ortega Rosas <19AlexOrtegaRosas97@users.noreply.github.com>
2020-06-09 16:06:31 -0500

    Finish Part form Login
    Finish Part form Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'package:form_validation/src/bloc/provider.dart';
+import 'package:form_validation/src/pages/home_page.dart';
+import 'package:form_validation/src/pages/login_page.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+    return Provider(
+      child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Form Validation App',
+      initialRoute: 'login',
+      routes: {
+        'login' : (BuildContext context) => LoginPage(),
+        'home'  : (BuildContext context) => HomePage(),
+      },
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
       ),
+    ),
     );
   }
 }
---
+++ lib/src/bloc/login_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'package:form_validation/src/bloc/validators.dart';
+import 'package:rxdart/rxdart.dart';
+
+class LoginBloc with Validators{
+
+  final _emailController    = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
+
+  //Insertar Valores al Stream
+  Function(String) get changeEmail    => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  //Recuperar datos Stream
+  Stream<String> get emailStream => _emailController.stream.transform(validarEmail);
+  Stream<String> get passwordStream => _passwordController.stream.transform(validarPassword);
+
+  Stream<bool> get formValidStream => CombineLatestStream.combine2(emailStream, passwordStream, (e, p) => true);
+
+  //Obtener el ultimo valor ingresado s los Streams
+
+  String get email => _emailController.value;
+  String get password => _passwordController.value;
+
+  dispose(){
+    _emailController?.close();
+    _passwordController?.close();
+  }
+}
